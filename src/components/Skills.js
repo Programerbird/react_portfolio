12f8502d8f25944f7faebed2f844a1944b66ab98
Water Palma <programerbird@gmail.com>
2023-10-18 21:31:07 -0700

    port_update_3

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import './Skills.css'
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import { color } from '@mui/system';
+import { colors } from '@mui/material';
+
 
 const Skills = () => {
   return (
     <section id="skills"> 
-    <div>
-       <div>
-        <h1 className='info' >hello</h1>
+    <div><h1 className='sk_name' >Skills</h1></div>
+      <div className='sk_wrapper' >
+        <div className='spam' >
+        <Box  component="span" sx={{ p: 1, border: '0.1px dashed grey' }}>
+        <Button style={{color:"yellow"}} ><JavascriptIcon/></Button>
+        </Box>
+        <Box  component="span" sx={{ p: 1, border: '0.1px dashed grey' }}>
+        <Button style={{color:"yellow"}} ><JavascriptIcon/></Button>
+        </Box>
+        <Box  component="span" sx={{ p: 1, border: '0.1px dashed grey' }}>
+        <Button style={{color:"yellow"}} ><JavascriptIcon/></Button>
+        </Box>
+        </div>
       </div>
-    </div>
   </section>
   )
 }
