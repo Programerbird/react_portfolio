5b5ebd27d96bba7865e4567656c00139eb75f9b9
Water Palma <programerbird@gmail.com>
2023-10-22 13:29:08 -0700

    update_6

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ const About = () => {
   return (
     <section id="about"> 
       <div className='ab_wrapper' >
+        <h1>About</h1>
          <img  src={pfp} alt='pfppic'/>
          <h1 className='info' >I'm a front end developer with one years of experience in web development.<br></br> I've worked on a variety of projects<br></br> I'm passionate about creating great user experiences and have a strong understanding of usability<br></br> and accessibility standards.</h1>
       </div>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,68 @@
 import React from 'react'
 import './Contact.css'
+import SendIcon from '@mui/icons-material/Send';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const Contact = () => {
   return (
     <section id="contact"> 
-    <div>
-       <div>
-        <h1 className='info' >hello</h1>
+    
+       <form>
+       <div className='contactmain'>
+      <h1>Contact</h1>
       </div>
-    </div>
+       <Box className='namet'
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Name" variant="outlined" />
+      </Box>
+     
+     
+      <Box className='namet'
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Email" variant="outlined" />
+      </Box>
+
+
+      <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div className='massaget' >
+      <TextField
+          id="filled-multiline-static"
+          label="Massage"
+          multiline
+          rows={4}
+          defaultValue=""
+          variant="filled"
+        />
+      </div >
+      
+      </Box>
+      <div className='buttont'  >
+      <Button  variant="contained" endIcon={<SendIcon />}>
+        Send
+     </Button>
+     </div>
+    </form>
   </section>
   )
 }
